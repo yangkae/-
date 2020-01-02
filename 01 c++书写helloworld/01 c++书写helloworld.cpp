55dc2750c796d920e2c596009ab539820be243f6
宁采晨 <yangkae@mail.ustc.edu.cn>
2020-01-02 16:54:42 +0800

    1

--- 01 c++书写helloworld/01 c++书写helloworld.cpp
+++ 01 c++书写helloworld/01 c++书写helloworld.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
 	cout << "hello world!" << endl;
+	cout << sizeof(int) << endl;
+	
+	float f1 = 3.141592653f;
+	cout << f1 << endl;
+	double d1 = 3.14;
+	cout << d1 << endl;
+
+	char ch = 'a';
+	cout << "charµÄ¿Õ¼ä" << sizeof(char)<<endl;
+	cout << "charµÄascii" <<(int)ch << endl;
+
+	string str = "hello";
+	cin >> str;
+	cout << str << endl;
 	system("pause");
 }
